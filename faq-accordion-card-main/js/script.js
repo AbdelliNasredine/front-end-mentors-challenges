3b90f9a5323956e6ec3db25dc1ab160e03225aa5
AbdelliNasredine <abdou.nasredine@gmail.com>
2020-11-06 23:58:07 +0100

    [faq-accordion-card-main]: finish mobile version

--- faq-accordion-card-main/js/script.js
+++
@@ -1,8 +0,0 @@
-var h2s = document.querySelectorAll("h2");
-
-h2s.forEach((h2) => {
-  h2.addEventListener("click", function (e) {
-    var parent = this.parentElement;
-    parent.classList.toggle("active");
-  });
-});
