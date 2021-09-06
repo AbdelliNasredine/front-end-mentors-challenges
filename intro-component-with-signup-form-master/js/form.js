356f1afffc4f1a5d7ca0411d479441eac1e7a446
AbdelliNasredine <abdou.nasredine@gmail.com>
2021-09-06 19:25:00 +0100

    [order-summary-component]: FINISHED

---
+++ intro-component-with-signup-form-master/js/form.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".form");
+console.log(form);
+form.addEventListener("submit", onSubmitHandler);
+
+function onSubmitHandler(event) {
+  // event.preventDefault();
+  // const error = validate();
+  // if(!error) form.submit();
+}
+function validate() {}
